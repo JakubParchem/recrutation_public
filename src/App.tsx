96793b0b1a1688a1b342b6d55a8233bd57453ae0
JakubParchem <127241475+JakubParchem@users.noreply.github.com>
2025-06-22 02:51:19 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import CountryList from './classes/countryList'
 import {StatsDisplay} from "./classes/statistics";
+//
 function App() {
     const localPage:string|null=localStorage.getItem('page');
     if(!localPage){
